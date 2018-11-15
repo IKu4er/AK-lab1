d2155dceb5386547a2bfd0a8aca6eed997153f43
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    Behind the curtain, in the pantomime

--- calculator.cpp
+++ calculator.cpp
@@ -9,3 +9,20 @@ int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+//Empty spaces, what are we living for?
+//Abandoned places, I guess we know the score, on and on
+//Does anybody know what we are looking for?
+//Another hero, another mindless crime
+//Behind the curtain, in the pantomime
+//Hold the line
+//Does anybody want to take it anymore?
+//
+//The show must go on
+//Inside my heart is breaking
+//My makeup may be flaking
+//But my smile, still, stays on
